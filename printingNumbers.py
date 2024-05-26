b1f38a51e22107faf7895693401f4951af3b9a0d
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-05-26 12:11:53 -0300

    printando numeros  e adições em py

---
+++ printingNumbers.py
@@ -0,0 +1,5 @@
+"""ASSIGNMENT
+It's common in videogames to do basic addition. For example, we may need to calculate the additional damage a sword deals when it is enchanted with a spell.
+Our hero's sword does 250 normally but does an additional 75 damage when it's enchanted.
+Calculate and print the result of 250 + 75."""
+print(250+75)
